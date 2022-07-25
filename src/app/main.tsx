afd3070174a83c5fe61a6544e9e74f32113a8e7c
Ling Chen <lingchen@Lings-MacBook-Pro.local>
2022-07-25 10:35:04 -0400

    chore: fix code style

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const el = document.getElementById('root')
-if (el === null) throw new Error('Root container missing in index.html')
+const el = document.getElementById('root');
+if (el === null) throw new Error('Root container missing in index.html');
 
-const root = ReactDOM.createRoot(el)
+const root = ReactDOM.createRoot(el);
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
